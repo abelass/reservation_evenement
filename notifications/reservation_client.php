a89a25b634b0325e6b664626c23d542f6f7eae82
Rainer Müller <rainer@websimple.be>
2013-05-28 18:38:47 +0200

    le bon teste

--- notifications/reservation_client.php
+++ notifications/reservation_client.php
@@ -3,8 +3,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function notifications_reservation_client_destinataires_dist($id_reservation, $options) {	
 	$donnees=sql_fetsel("id_auteur,email","spip_reservations","id_reservation=".$id_reservation);
-    if(isset($donnees['email']))$dest=$donnees['email'];
-    else $dest=$donnees['id_auteur'];
+    if(intval($donnees['id_auteur']))$dest=$donnees['id_auteur'];
+    else $dest=$donnees['email'];
 	return array($dest);	
 }
 
