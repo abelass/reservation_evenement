02f281a7724f356252a945fe68effc7c392626c5
Rainer Müller <rainer@websimple.be>
2013-11-06 18:49:10 +0100

    meilleur déscriptif

--- lang/paquet-reservation_evenement_fr.php
+++ lang/paquet-reservation_evenement_fr.php
@@ -6,9 +6,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
-	'reservation_evenement_description' => 'Gestionnaire de réservation d\'événements',
+	'reservation_evenement_description' => 'Ce plugin, qui nécessite le plugin agenda propose une gestion avancée de réservation d’événements.
+
+Il contient :
+- Un formulaire avec sélection d\'événements multiple, données personnelles extensible via le plugin champs extras
+- La gestion des réservation et les détails de réservation
+- Exportation via csv
+- Gestion d\'envoi d\'email automatique selon statut de la réservation (à définir dans le panneau de configuration)',
 	'reservation_evenement_nom' => 'Réservation Événements',
-	'reservation_evenement_slogan' => 'Réserver des événements',
+	'reservation_evenement_slogan' => 'Gestionnaire de réservation d\'événements',
 );
 
 ?>
