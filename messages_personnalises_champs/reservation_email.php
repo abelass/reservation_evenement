0eef5e293d844c6f656836ec645aa54653308116
abelass <rainer@websimple.be>
2018-02-23 11:41:46 +0100

    bonne définitions

--- messages_personnalises_champs/reservation_email.php
+++ messages_personnalises_champs/reservation_email.php
@@ -1,8 +1,8 @@
 <?php
 
-function messages_personnalises_champs_reservation_nom_dist($nom, $data_objet) {
+function messages_personnalises_champs_reservation_email_dist($nom, $data_objet) {
 	if ($data_objet['reservation_id_auteur'] > 0) {
-		$nom = mp_chercher_valeur_champ('auteur_nom', $data_objet['auteur_nom'], $data_objet);
+		$nom = mp_chercher_valeur_champ('auteur_email', $data_objet['auteur_email'], $data_objet);
 	}
 
 	return $nom;
