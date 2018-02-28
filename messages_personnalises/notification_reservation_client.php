7745adc62adf366f14e32f064db5e61006e58c9a
abelass <rainer@websimple.be>
2018-02-28 15:27:52 +0100

    - Renommage
    les notifications doivent porter le nom de l'Événement de notification

--- messages_personnalises/notification_reservation_client.php
+++ messages_personnalises/notification_reservation_client.php
@@ -16,7 +16,7 @@
  *        	Variables du contexte.
  * @return array Définition.
  */
-function messages_personnalises_notification_reservation_dist($args) {
+function messages_personnalises_notification_reservation_client_dist($args) {
 
 	// Les champs reservations
 	$reservations = lister_tables_objets_sql('spip_reservations');
